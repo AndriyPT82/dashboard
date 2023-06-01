89aa39545889fdead7717a1288ad815320f0796b
Andriy Ivanyk <andriyivanyk82@gmail.com>
2023-06-01 17:54:30 +0300

    fixed Go back button

--- src/components/DashboardHeader/DashboardHeader.jsx
+++ src/components/DashboardHeader/DashboardHeader.jsx
@@ -1,21 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from '../../utils/icons/Rendersee-logo.png';
 import './DashboardHeader.scss';
 
 export function DashboardHeader() {
+
+  const navigate = useNavigate()
+
   return (
     <div className="dashboard-header">
-    <div className="dashboard-header__logo-block">
-      <img src={logo} alt="dashboard-logo" />
+      <div className="dashboard-header__logo-block">
+        <img src={logo} alt="dashboard-logo" />
       </div>
 
       <div className="dashboard-header__container">
-      <Link to={"/"} className="dashboard-header__go-back">Go back</Link>
-      <div className="dashboard-header__create-project-block">
-        <button className="dashboard-header__new-project-button"> Create New Project</button>
-        <button className="dashboard-header__bell"></button>
-      </div>
+        <button
+
+          className="dashboard-header__go-back"
+          onClick={() => navigate(-1)}
+        >
+          Go back
+        </button>
+        <div className="dashboard-header__create-project-block">
+          <button className="dashboard-header__new-project-button"> Create New Project</button>
+          <button className="dashboard-header__bell"></button>
+        </div>
       </div>
     </div>
   );
